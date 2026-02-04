8cfed0a1dd5ffa7a95f813a9ae2b9e6e3d23d92b
ArsanyEhab711 <arsanyehab711@gmail.com>
2026-02-04 09:45:21 +0200

    fix error in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar' // 👈 متنساش تستدعي الناف بار
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
 import CategoryPage from './pages/CategoryPage'
-import About from './pages/About'
-import Footer from './components/Footer'
-
-// ... الاستيرادات بتاعتك
+import About from './pages/About' // 👈 وتستدعي باقي الصفحات
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col"> {/* 👈 ضيفنا flex عشان الفوتر ينزل تحت خالص لو الصفحة فاضية */}
-      
-      {/* 1. الناف بار ثابت فوق */}
-      <Navbar /> 
+    // 👇 لازم الـ Router يبدأ من هنا ويحوط التطبيق كله
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        
+        {/* 1. الناف بار */}
+        <Navbar /> 
 
-      {/* 2. المحتوى المتغير (الصفحات) */}
-      <div className="flex-grow"> {/* 👈 الكلاس ده مهم عشان يزق الفوتر لتحت */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/:category" element={<CategoryPage />} />
-          {/* ... باقي المسارات ... */}
-        </Routes>
+        {/* 2. المحتوى المتغير */}
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/shop/:category" element={<CategoryPage />} />
+            {/* ضيف مسار الـ About لو مش ضايفه */}
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+
+        {/* 3. الفوتر */}
+        <Footer /> 
+
       </div>
-
-      {/* 3. الفوتر ثابت تحت في كل الصفحات */}
-      <Footer /> 
-
-    </div>
+    </Router>
   )
 }
+
 export default App
