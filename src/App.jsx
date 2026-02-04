c0c477f2147f6926ae30d69a382cb655a0c88134
ArsanyEhab711 <arsanyehab711@gmail.com>
2026-02-04 10:21:47 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar' // 👈 متنساش تستدعي الناف بار
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
 import CategoryPage from './pages/CategoryPage'
-import About from './pages/About' // 👈 وتستدعي باقي الصفحات
+import About from './pages/About'
+import Developer from './pages/Developer' // 👈 1. ضيف الاستيراد ده
 
 function App() {
   return (
-    // 👇 لازم الـ Router يبدأ من هنا ويحوط التطبيق كله
     <Router>
       <div className="min-h-screen flex flex-col">
         
-        {/* 1. الناف بار */}
         <Navbar /> 
 
-        {/* 2. المحتوى المتغير */}
         <div className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/shop/:category" element={<CategoryPage />} />
-            {/* ضيف مسار الـ About لو مش ضايفه */}
             <Route path="/about" element={<About />} />
+            
+            {/* 👇 2. ضيف السطر ده عشان صفحة المطور تفتح */}
+            <Route path="/developer" element={<Developer />} />
+            
           </Routes>
         </div>
 
-        {/* 3. الفوتر */}
         <Footer /> 
 
       </div>
