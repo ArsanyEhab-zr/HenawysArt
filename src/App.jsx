b1743846516fef72850151718ab276da0b230306
ArsanyEhab711 <arsanyehab711@gmail.com>
2026-02-07 02:57:55 +0200

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Home from './pages/Home'
 import Shop from './pages/Shop'
 import CategoryPage from './pages/CategoryPage'
 import About from './pages/About'
-import Developer from './pages/Developer' // 👈 1. ضيف الاستيراد ده
+import Contact from './pages/Contact' // 👈 1. لازم تضيف السطر ده!
+import Developer from './pages/Developer'
 
 function App() {
   return (
@@ -20,11 +21,11 @@ function App() {
             <Route path="/shop" element={<Shop />} />
             <Route path="/shop/:category" element={<CategoryPage />} />
             <Route path="/about" element={<About />} />
+            
+            {/* دلوقتي ده هيشتغل صح لأننا عملنا import فوق */}
             <Route path="/contact" element={<Contact />} />
             
-            {/* 👇 2. ضيف السطر ده عشان صفحة المطور تفتح */}
             <Route path="/developer" element={<Developer />} />
-            
           </Routes>
         </div>
 
